22f46c37bd6aef7a5c83c07efbc286da06443322
McIvor (Ki) Steiner <mcivorsteiner@gmail.com>
2017-08-26 11:44:20 -0700

    Initial Commit

---
+++ LocalBeatStart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalBeatStart
+//
+//  Created by Mcivor Steiner on 8/26/17.
+//  Copyright © 2017 Mcivor Steiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
